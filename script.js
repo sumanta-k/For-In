1e7b704ffc82c5f7903322bc0695ecc33a777af0
sumanta kumar mohanta <mohantasumanta601@gmail.com>
2023-09-05 22:12:21 +0530

    iterate through for in loop

---
+++ script.js
@@ -0,0 +1,9 @@
+const colorObj ={
+    color1 :'red',
+    color2 :'blue',
+    color3: 'orange',color4:'green'
+};
+//for in - iterate 
+for (const key in colorObj){
+    console.log(key, colorObj[key]);
+}
